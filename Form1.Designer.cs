a768bbfeb6bc273390489366e203720440ff07ad
VDHGG <vuduchuy2072004@gmail.com>
2024-10-31 16:12:08 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace BaiKTSo2_31_10_2024
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.XoaButton = new System.Windows.Forms.Button();
+            this.ThemButton = new System.Windows.Forms.Button();
+            this.ListSPBox = new System.Windows.Forms.ListBox();
+            this.GioHangBox = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TextTotal = new System.Windows.Forms.TextBox();
+            this.ThanhToanButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // XoaButton
+            // 
+            this.XoaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.XoaButton.Location = new System.Drawing.Point(560, 343);
+            this.XoaButton.Name = "XoaButton";
+            this.XoaButton.Size = new System.Drawing.Size(178, 32);
+            this.XoaButton.TabIndex = 25;
+            this.XoaButton.Text = "XÓA KHỎI GIỎ HÀNG";
+            this.XoaButton.UseVisualStyleBackColor = true;
+            // 
+            // ThemButton
+            // 
+            this.ThemButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ThemButton.Location = new System.Drawing.Point(216, 343);
+            this.ThemButton.Name = "ThemButton";
+            this.ThemButton.Size = new System.Drawing.Size(223, 32);
+            this.ThemButton.TabIndex = 24;
+            this.ThemButton.Text = "THÊM VÀO GIỎ HÀNG";
+            this.ThemButton.UseVisualStyleBackColor = true;
+            this.ThemButton.Click += new System.EventHandler(this.ThemButton_Click);
+            // 
+            // ListSPBox
+            // 
+            this.ListSPBox.FormattingEnabled = true;
+            this.ListSPBox.ItemHeight = 16;
+            this.ListSPBox.Location = new System.Drawing.Point(143, 6);
+            this.ListSPBox.Name = "ListSPBox";
+            this.ListSPBox.Size = new System.Drawing.Size(707, 292);
+            this.ListSPBox.TabIndex = 32;
+            // 
+            // GioHangBox
+            // 
+            this.GioHangBox.FormattingEnabled = true;
+            this.GioHangBox.ItemHeight = 16;
+            this.GioHangBox.Location = new System.Drawing.Point(873, 6);
+            this.GioHangBox.Name = "GioHangBox";
+            this.GioHangBox.Size = new System.Drawing.Size(486, 292);
+            this.GioHangBox.TabIndex = 33;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(918, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 16);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "TỔNG TIỀN:";
+            // 
+            // TextTotal
+            // 
+            this.TextTotal.Location = new System.Drawing.Point(1008, 343);
+            this.TextTotal.Name = "TextTotal";
+            this.TextTotal.Size = new System.Drawing.Size(325, 22);
+            this.TextTotal.TabIndex = 35;
+            // 
+            // ThanhToanButton
+            // 
+            this.ThanhToanButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ThanhToanButton.Location = new System.Drawing.Point(1025, 415);
+            this.ThanhToanButton.Name = "ThanhToanButton";
+            this.ThanhToanButton.Size = new System.Drawing.Size(178, 32);
+            this.ThanhToanButton.TabIndex = 36;
+            this.ThanhToanButton.Text = "THANH TOÁN";
+            this.ThanhToanButton.UseVisualStyleBackColor = true;
+            this.ThanhToanButton.Click += new System.EventHandler(this.ThanhToanButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1427, 757);
+            this.Controls.Add(this.ThanhToanButton);
+            this.Controls.Add(this.TextTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.GioHangBox);
+            this.Controls.Add(this.ListSPBox);
+            this.Controls.Add(this.XoaButton);
+            this.Controls.Add(this.ThemButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button XoaButton;
+        private System.Windows.Forms.Button ThemButton;
+        private System.Windows.Forms.ListBox ListSPBox;
+        private System.Windows.Forms.ListBox GioHangBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TextTotal;
+        private System.Windows.Forms.Button ThanhToanButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Menu;
+
+namespace BaiKTSo2_31_10_2024
+{
+    public partial class Form1 : Form
+    {
+        public class SanPham
+        {
+            public string TenSP { get; set; }
+            public decimal GiaSP { get; set; }
+            public int SoLuongSP { get; set; }
+
+            public SanPham(string tensp, decimal giasp, int soluongsp)
+            {
+                TenSP = tensp;
+                GiaSP = giasp;
+                SoLuongSP = soluongsp;
+            }
+        }
+
+        public class GioHang
+        {
+            private Dictionary<SanPham, int> TrongGioHang = new Dictionary<SanPham, int>();
+            public void ThemVaoGioHang(SanPham product, int quantity)
+            {
+                if (TrongGioHang.ContainsKey(product))
+                    TrongGioHang[product] += quantity;
+                else
+                    TrongGioHang[product] = quantity;
+            }
+
+            public void XoaKhoiGioHang(SanPham product)
+            {
+                if (TrongGioHang.ContainsKey(product))
+                    TrongGioHang.Remove(product);
+            }
+
+            public decimal CalculateTotal()
+            {
+                return TrongGioHang.Sum(item => item.Key.GiaSP * item.Value);
+            }
+
+            public void ClearCart()
+            {
+                TrongGioHang.Clear();
+            }
+
+            public Dictionary<SanPham, int> GetCartItems()
+            {
+                return TrongGioHang;
+            }
+        }
+
+        private List<SanPham> products = new List<SanPham>();
+        private GioHang cart = new GioHang();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadProducts();
+            DisplayProducts();
+        }
+
+        private void LoadProducts()
+        {
+            products.Add(new SanPham("AO BA LO", 100000, 10));
+            products.Add(new SanPham("QUAN DUI", 200000, 15));
+            products.Add(new SanPham("VAY", 150000, 20));
+            products.Add(new SanPham("AO DAI", 250000, 30));
+        }
+
+        private void DisplayProducts()
+        {
+            ListSPBox.Items.Clear();
+            foreach (var product in products)
+            {
+                ListSPBox.Items.Add($"{product.TenSP} - {product.GiaSP:C} - So Luong Con Lai: {product.SoLuongSP}");
+            }
+        }
+
+        private void DisplayCartItems()
+        {
+            GioHangBox.Items.Clear();
+            foreach (var item in cart.GetCartItems())
+            {
+                GioHangBox.Items.Add($"{item.Key.TenSP} - {item.Value} x {item.Key.GiaSP:C}");
+            }
+        }
+
+        private void UpdateTotal()
+        {
+            TextTotal.Text = $"{cart.CalculateTotal():C}";
+        }
+
+        private void ThemButton_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = ListSPBox.SelectedIndex;
+            if (selectedIndex >= 0 && selectedIndex < products.Count)
+            {
+                SanPham selectedProduct = products[selectedIndex];
+
+                if (selectedProduct.SoLuongSP > 0)
+                {
+                    cart.ThemVaoGioHang(selectedProduct, 1);
+                    selectedProduct.SoLuongSP--;
+                    DisplayProducts();
+                    DisplayCartItems();
+                    UpdateTotal();
+                }
+                else
+                {
+                    MessageBox.Show("Sản phẩm đã hết hàng.", "Thông báo");
+                }
+            }
+        }
+
+        private void ThanhToanButton_Click(object sender, EventArgs e)
+        {
+            if (cart.CalculateTotal() == 0)
+            {
+                MessageBox.Show("XIN HÃY CHỌN SẢN PHẨM CẦN THANH TOÁN", "Thông Báo");
+            }
+            else
+            {
+                MessageBox.Show("THANH TOÁN THÀNH CÔNG", "Thông Báo");
+                cart.ClearCart();
+                DisplayCartItems();
+                UpdateTotal();
+            }
+        }
+
+
+        private void XoaButton_Click(object sender, EventArgs e)
+        {
+            if (GioHangBox.SelectedIndex >= 0)
+            {
+                int index = GioHangBox.SelectedIndex;
+
+                string selectedText = GioHangBox.Items[index].ToString();
+                string productName = selectedText.Split('-')[0].Trim(); 
+
+               
+                var selectedCartItem = cart.GetCartItems()
+                    .FirstOrDefault(item => item.Key.TenSP == productName);
+
+                if (selectedCartItem.Key != null)
+                {
+                    
+                    selectedCartItem.Key.SoLuongSP += selectedCartItem.Value;
+
+                    cart.XoaKhoiGioHang(selectedCartItem.Key);
+
+            
+                    DisplayProducts();
+                    DisplayCartItems();
+                    UpdateTotal();
+                }
+                else
+                {
+                    MessageBox.Show("Sản phẩm không tìm thấy trong giỏ hàng.", "Thông báo");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn sản phẩm để xóa.", "Thông báo");
+            }
+        }
+    }
+}
